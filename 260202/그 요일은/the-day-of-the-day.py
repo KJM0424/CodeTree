9d0c92c50685531442fc56649664bf5d3b1b08dc
KJM0424 <143463633+KJM0424@users.noreply.github.com>
2026-02-02 22:01:39 +0900

    Title: 그 요일은, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260202/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,25 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+# Please write your code here.
+days_of_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+week_days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+def count_days(m, d):
+    total_days = 0
+    for i in range(1, m):
+        total_days += days_of_month[i]
+    total_days += d
+    return total_days
+
+diff = count_days(m2, d2) - count_days(m1, d1)
+
+target_idx = week_days.index(A)
+
+ans = 0
+
+for i in range(diff + 1):
+    if i % 7 == target_idx:
+        ans += 1
+
+print(ans)
