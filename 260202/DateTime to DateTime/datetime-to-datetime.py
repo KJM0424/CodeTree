11556c759026331c1bae74a5cdd0f4f8c851b334
KJM0424 <143463633+KJM0424@users.noreply.github.com>
2026-02-02 21:35:17 +0900

    Title: DateTime to DateTime, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260202/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,12 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+start_day = 11
+start_hour = 11
+start_min = 11
+start_total = (start_day - 1) * 24 * 60 + start_hour * 60 + start_min
+
+end_total = (a - 1) * 24 * 60 + b * 60 + c
+
+result = end_total - start_total
+print(result)
