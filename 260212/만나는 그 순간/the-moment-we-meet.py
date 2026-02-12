cc1678e909c6ce13b9230a103ec322cc052919d1
KJM0424 <143463633+KJM0424@users.noreply.github.com>
2026-02-12 23:02:11 +0900

    Title: 만나는 그 순간, Time: 105ms, Memory: 28MB, Status: Passed - Codetree

--- 260212/만나는 그 순간/the-moment-we-meet.py
+++ 260212/만나는 그 순간/the-moment-we-meet.py
@@ -14,20 +14,17 @@ for _ in range(m):
     d2.append(direction)
     t2.append(int(time))
 
-# Please write your code here.
-pos_a = [0] # 0초 위치
+pos_a = [0]
 current = 0
 
 for i in range(n):
-    # 방향에 따라 1초당 이동할 거리 설정
+
     step = 1 if d[i] == 'R' else -1
     
-    # 시간(t[i])만큼 반복하며 매 초마다 위치 기록
     for _ in range(t[i]):
         current += step
         pos_a.append(current)
 
-# --- B의 이동 경로 기록 ---
 pos_b = [0]
 current = 0
 
@@ -38,9 +35,7 @@ for i in range(m):
         current += step
         pos_b.append(current)
 
-# --- 최초로 만나는 시간 찾기 ---
 ans = -1
-# 0초는 제외하고 1초부터 비교
 for i in range(1, len(pos_a)):
     if pos_a[i] == pos_b[i]:
         ans = i
